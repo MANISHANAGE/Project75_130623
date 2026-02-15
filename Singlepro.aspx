cae2f11fae755bf66b8df84b2a23818e0a284bd0
Manisha <Manisha@LAPTOP-8E1LD448>
2026-02-15 10:54:37 +0530

    Add project files.

---
+++ Singlepro.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Singlepro.aspx.cs" Inherits="Project75_130623.Singlepro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 style="color:mediumvioletred;">Employee Details</h1>
+            
+            <table border="1" style="background-color:darkblue;color:white" width="500px">
+            <tr>
+                <td>Name:</td>
+                <td><asp:TextBox ID="txtname" runat="server" width="200px"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Age:</td>
+                <td><asp:TextBox ID="txtage" runat="server" width="50px"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>City:</td>
+                <td><asp:TextBox ID="txtcity" runat="server" width="200px"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Salary:</td>
+                <td><asp:TextBox ID="txtsal" runat="server" width="200px"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><asp:Button ID="btnsave" runat="server" Text="Save" Onclick="btnsave_Click"  forecolor="Black" Font-Size="Large" BackColor="deeppink"/></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><asp:Gridview ID="Gvemp" runat="server" AutoGenerateColumns="false" OnRowCommand="Gvemp_RowCommand">
+                    <Columns>
+                        <asp:TemplateField HeaderText="Emp id">
+                            <ItemTemplate>
+                                <%#Eval("id")%>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Emp Name">
+                            <ItemTemplate>
+                                <%#Eval("Name")%>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Emp Age">
+                            <ItemTemplate>
+                                <%#Eval("Age")%>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Emp City">
+                            <ItemTemplate>
+                                <%#Eval("City")%>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Emp Salary">
+                            <ItemTemplate>
+                                <%#Eval("Salary")%>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField>
+                            <ItemTemplate>
+                                <asp:LinkButton ID="btndelete" runat="server" Text="Delete"  ForeColor="white" CommandName="A" CommandArgument='<%#Eval("id")%>'></asp:LinkButton>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField>
+                            <ItemTemplate>
+                                <asp:LinkButton ID="btnedit" runat="server" Text="Edit" ForeColor="white" CommandName="B" CommandArgument='<%#Eval("id")%>'></asp:LinkButton>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                    </asp:Gridview></td>
+            </tr>
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Singlepro.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project75_130623
+{
+    public partial class Singlepro : System.Web.UI.Page
+    {
+        //SqlConnection con = new SqlConnection("Data Source=LAPTOP-8E1LD448\\SQLEXPRESS;initial catalog=db75_130623;integrated security=true");
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-8E1LD448\\SQLEXPRESS;initial catalog=db75_130623_2026feb;integrated security=true");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Show();
+        }
+        public void Show()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Empsinglepro", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@action", "select");
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            Gvemp.DataSource = dt;
+            Gvemp.DataBind();
+        }
+        public void Clear()
+        {
+            txtname.Text = "";
+            txtcity.Text = "";
+            txtage.Text = "";
+            txtsal.Text = "";
+            btnsave.Text = "Save";
+        }
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Empsinglepro", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action","insert");
+                cmd.Parameters.AddWithValue("@Name", txtname.Text);
+                cmd.Parameters.AddWithValue("@Age", txtage.Text);
+                cmd.Parameters.AddWithValue("@City", txtcity.Text);
+                cmd.Parameters.AddWithValue("@Salary", txtsal.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Show();
+                Clear();
+            }
+            else if (btnsave.Text == "Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Empsinglepro", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action","update");
+                cmd.Parameters.AddWithValue("@Name", txtname.Text);
+                cmd.Parameters.AddWithValue("@Age", txtage.Text);
+                cmd.Parameters.AddWithValue("@City", txtcity.Text);
+                cmd.Parameters.AddWithValue("@Salary", txtsal.Text);
+                cmd.Parameters.AddWithValue("@id", ViewState["pp"]);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Show();
+                Clear();
+            }
+        }
+
+        protected void Gvemp_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Empsinglepro", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action","delete");
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Show();
+            }
+            else if (e.CommandName == "B")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Empsinglepro", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action","edit");
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0]["Name"].ToString();
+                txtage.Text = dt.Rows[0]["Age"].ToString();
+                txtcity.Text = dt.Rows[0]["City"].ToString();
+                txtsal.Text = dt.Rows[0]["Salary"].ToString();
+                btnsave.Text = "Update";
+                ViewState["pp"] = e.CommandArgument;
+
+
+            }
+        }
+
+
+    }
+}
